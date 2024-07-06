7cfee80454ed2560c56e46528556ff823e7231a7
Aditya Raj <aditya.raj@mirketa.com>
2024-07-06 22:32:08 +0530

    update the docstrings & add asserts

--- src/sf_utils/sf_utils.py
+++ src/sf_utils/sf_utils.py
@@ -7,13 +7,20 @@ import fire
 
 
 class SfUtils(object):
-    """Various utilities built around sf-cli"""
+    """
+    Various utilities built around sf-cli
+    """
 
     def reauth(self, target_org: str = None):
-        """Reauthenticate the given org
+        """
+        This command re-authenticates a sf-cli authenticated org.
 
-        Keyword arguments:
-        target_org -- org-alias to reauthenticate
+        Often we need to re-authenticate an org with the same alias & credentials, due to expired refresh tokens etc.
+        By default, This command re-authenticates the default org, if ran in a project folder. 
+        Or Optionally, you can pass alias of the org which you want to re-authenticate.
+
+
+        :param target_org: If present, re-authenticates the org with given alias instead of the default org
         """
 
         org_details_command = ["sf", "org", "display"]
@@ -29,7 +36,11 @@ class SfUtils(object):
                 subprocess.run(
                     org_details_command, check=True, shell=True, capture_output=True
                 ).stdout
-            )["result"]
+            )
+
+            assert "result" in org_details.keys(), "'Result' not found in Org Details!"
+            org_details = org_details["result"]
+
         except subprocess.CalledProcessError as e:
             if target_org:
                 print(f"FATAL: Failed to get org details of '{target_org}'")
@@ -39,9 +50,10 @@ class SfUtils(object):
                 print(e)
             sys.exit(1)
 
-        org_alias = ""
+        assert "alias" in org_details.keys(), "'alias' not found in org details!"
+        org_alias = org_details["alias"]
+
         try:
-            org_alias = org_details["alias"]
             print(f"Logging out of '{org_alias}'...")
             org_logout_command = [
                 "sf",
@@ -58,10 +70,11 @@ class SfUtils(object):
             print(f"FATAL: Failed to logout of '{org_alias}'")
             print(e)
             sys.exit(1)
-        except KeyError:
-            print("FATAL: Couldn't find 'alias' in org details")
-            print(org_details)
-            sys.exit(1)
+
+        assert (
+            "instanceUrl" in org_details.keys()
+        ), "'instanceUrl' not found in org details!"
+        org_instance_url = org_details["instanceUrl"]
 
         try:
             print(f"Logging back into '{org_alias}'...")
@@ -73,7 +86,7 @@ class SfUtils(object):
                 "--alias",
                 org_alias,
                 "--instance-url",
-                org_details["instanceUrl"],
+                org_instance_url,
             ]
 
             if not target_org:
@@ -87,12 +100,9 @@ class SfUtils(object):
         except subprocess.CalledProcessError as e:
             print(f"FATAL: Failed to authenticate {org_alias}")
             sys.exit(1)
-        except KeyError:
-            print("FATAL: Couldn't find 'instanceUrl' in org details")
-            sys.exit(1)
 
         print(f"Re-authenticated '{org_alias}' Successfully!")
-        print("IMPORTANT: Please restart VS Code, if open, for changes to take effect!")
+        print("IMPORTANT: Please restart the VS Code, if still getting authentication errors!")
 
 
 def __sf_utils__():
